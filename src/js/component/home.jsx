a329f1ff8e3ae89a7ff2ed3f60fcdabf753cef0d
Agustin1112 Agustin <agustinperrone620@gmail.com>
2024-09-30 23:58:40 +0000

    Proyecto simple counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,26 @@
 import React from "react";
-import Card from "./card";
-import Jumbotron from "./jumbotron";
-import Navbar from "./navbar";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
 const Home = () => {
-  return (
-    <>
-      <Navbar />
-      <Jumbotron />
-      <div className="container py-5">
-        <div className="row">
-          <Card title="Card title 1" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit." imgSrc="https://via.placeholder.com/500x325" />
-          <Card title="Card title 2" text="Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." imgSrc="https://via.placeholder.com/500x325" />
-          <Card title="Card title 3" text="Ut enim ad minim veniam, quis nostrud exercitation ullamco." imgSrc="https://via.placeholder.com/500x325" />
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<div className="text-center">
+			<h1 className="text-center mt-5">Hello Rigo!</h1>
+			<p>
+				<img src={rigoImage} />
+			</p>
+			<a href="#" className="btn btn-success">
+				If you see this green button... bootstrap is working...
+			</a>
+			<p>
+				Made by{" "}
+				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+				love!
+			</p>
+		</div>
+	);
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,51 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "../styles/index.css";
-import Home from "./component/home.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PropTypes from 'prop-types';
+import '../styles/index.css';
 
-ReactDOM.createRoot(document.getElementById('app')).render(<Home />);
+// Componente SimpleCounter
+function SimpleCounter(props) {
+    return (
+        <div className="bigCounter">
+            <div className="calendar">
+                <i className="far fa-clock"></i>
+            </div>
+            <div className="number">{props.digitFour % 10}</div>
+            <div className="number">{props.digitThree % 10}</div>
+            <div className="number">{props.digitTwo % 10}</div>
+            <div className="number">{props.digitOne % 10}</div>
+        </div>
+    );
+}
+
+// Defino las prop types
+SimpleCounter.propTypes = {
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number,
+};
+
+// Lógica del contador
+let counter = 0;
+const root = ReactDOM.createRoot(document.querySelector('#app'));
+
+// Actualiza cada 200 ms
+setInterval(() => {
+    // Calcula los dígitos
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor((counter / 100) % 10);
+    const two = Math.floor((counter / 10) % 10);
+    const one = Math.floor(counter % 10);
+    
+    // Incremento el contador
+    counter += 2; // Cambia a 2 para que sea más rápido
+
+    // Renderizo la aplicación React
+    root.render(
+        <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />
+    );
+}, 200); // Cambiado a 200 para contar cada 200 ms
+
 
 
